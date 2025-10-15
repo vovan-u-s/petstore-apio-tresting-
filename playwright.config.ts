dd31a38be5878f790c759aa281628870433c57f5
vovan-u-s <oversoul6661@gmail.com>
2025-10-14 19:39:58 -0500

    get the user by username

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://jsonplaceholder.typicode.com',
+    baseURL: `${process.env.BASE_URL}${process.env.API_VERSION}`,
     
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
---
+++ tests/userAPis.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+import { fa, faker, Faker } from '@faker-js/faker';
+import { z } from 'zod';
+import { env } from 'process';
+
+
+test.describe('User API tests', () => { 
+
+    test('create a new user', async ({ request }) => {
+        const createUserRequestBody = {
+
+            "id": 12312,
+            "username": "TestUserNameSalih123",
+            "firstName": faker.person.firstName(),
+            "lastName": faker.person.lastName(),
+            "email": faker.internet.email(),
+            "password": "Test1234!",
+            "phone": faker.phone.number(),
+            "userStatus": 0
+        }
+        const createUserResponse = await request.post(`${process.env.BASE_URL}${process.env.API_VERSION}/user`, {
+            data: createUserRequestBody
+        })
+        expect(createUserResponse.status()).toBe(200);
+        const expectedResponseSchemaZod = z.object({
+            "code": z.literal(200),
+            "type": z.literal("unknown"),
+            "message": z.literal(createUserRequestBody.id.toString())
+        })
+        const actualResponseBody = await createUserResponse.json();
+        expectedResponseSchemaZod.parse(actualResponseBody);
+    })
+    test('get user by username', async ({ request }) => {
+        const username = "TestUserNameSalih123";
+        const getUserResponse = await request.get(`${process.env.BASE_URL}${process.env.API_VERSION}/user/${username}`);
+        expect(getUserResponse.status()).toBe(200);
+        const expectedGetUserResponseSchemaZod = z.object({
+            "id": z.number(),
+            "username": z.literal(username),
+            "firstName": z.string(),
+            "lastName": z.string(),
+            "email": z.string().email(),
+            "password": z.string(),
+            "phone": z.string(),
+            "userStatus": z.number()
+        })
+        const actualGetUserResponseBody = await getUserResponse.json();
+        expectedGetUserResponseSchemaZod.parse(actualGetUserResponseBody);
+
+    })
+})
