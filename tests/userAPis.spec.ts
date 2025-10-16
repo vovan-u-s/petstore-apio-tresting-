2f14c99c7d24ae4fe68da444179169f5f73d1b21
vovan-u-s <oversoul6661@gmail.com>
2025-10-15 20:37:55 -0500

    implement task for Sal

--- tests/userAPis.spec.ts
+++ tests/userAPis.spec.ts
@@ -3,6 +3,7 @@ import { fa, faker, Faker } from '@faker-js/faker';
 import { z } from 'zod';
 import { env } from 'process';
 import { getAPI, postAPI, deleteAPI, putAPI } from '../utils/apiCallHelper';
+import { APIRequestContext } from '@playwright/test';
 // Construct the base URL from environment variables
 const baseURL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
 
@@ -17,45 +18,46 @@ test.describe('User API tests', () => {
         "phone": faker.phone.number(),
         "userStatus": 0
     }
+    // Remove direct usage of request.post here; use postAPI in the test blocks instead.
+    const expectedResponseSchemaZod = z.object({
+        "code": z.literal(200),
+        "type": z.literal("unknown"),
+        "message": z.literal(createUserRequestBody.id.toString())
+    })
+    const username = createUserRequestBody.username;
+    const expectedGetUserResponseSchemaZod = z.object({
+    // Remove direct usage of request.get here; use getAPI in the test blocks instead.
+        "username": z.literal(username),
+        "firstName": z.string(),
+        "lastName": z.string(),
+        "email": z.string().email(),
+        "password": z.string(),
+        "phone": z.string(),
+        "userStatus": z.number()
+    })
+    const expectedDeleteUserResponseSchemaZod = z.object({
+    // Remove direct usage of request.delete here; use deleteAPI in the test blocks instead.
+        "type": z.literal("unknown"),
+        "message": z.literal(username)
+    })
+
     test('create a new user', async ({ request }) => {
-        const createUserResponse = await request.post(`${baseURL}/user`, {
-            data: createUserRequestBody
-        })
-        const expectedResponseSchemaZod = z.object({
-            "code": z.literal(200),
-            "type": z.literal("unknown"),
-            "message": z.literal(createUserRequestBody.id.toString())
-        })
-        await postAPI(request, `${baseURL}/user`, createUserRequestBody, 200, expectedResponseSchemaZod, 5);
+
+        await postAPI(request, `${baseURL}/user`, createUserRequestBody, 200, expectedResponseSchemaZod);
 
 
     })
     test('get user by username', async ({ request }) => {
-        const username = createUserRequestBody.username;
-        const getUserResponse = await request.get(`${baseURL}/user/${username}`);
-        const expectedGetUserResponseSchemaZod = z.object({
-            "id": z.number(),
-            "username": z.literal(username),
-            "firstName": z.string(),
-            "lastName": z.string(),
-            "email": z.string().email(),
-            "password": z.string(),
-            "phone": z.string(),
-            "userStatus": z.number()
-        })
-        await getAPI(request, `${baseURL}/user/${username}`, 200, expectedGetUserResponseSchemaZod, 5);
+        await getAPI(request, `${baseURL}/user/${username}`, 200, expectedGetUserResponseSchemaZod);
 
     })
 
+    test('update user by username', async ({ request }) => {
+        await putAPI(request, `${baseURL}/user/${username}`, createUserRequestBody, 200, expectedResponseSchemaZod);
+    })
     test('delete user by username', async ({ request }) => {
-        const username = createUserRequestBody.username;
-        const deleteUserResponse = await request.delete(`${baseURL}/user/${username}`);
-        const expectedDeleteUserResponseSchemaZod = z.object({
-            "code": z.literal(200),
-            "type": z.literal("unknown"),
-            "message": z.literal(username)
-        })
-        await deleteAPI(request, `${baseURL}/user/${username}`, 200, expectedDeleteUserResponseSchemaZod, 5);
+
+        await deleteAPI(request, `${baseURL}/user/${username}`, 200, expectedDeleteUserResponseSchemaZod);
 
     })
 
