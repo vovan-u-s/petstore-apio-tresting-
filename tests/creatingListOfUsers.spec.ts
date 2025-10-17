5d60f223d44eacf29af28d7cfe1241027a18ad83
vovan-u-s <oversoul6661@gmail.com>
2025-10-17 18:46:28 -0500

    with creating random users

--- tests/creatingListOfUsers.spec.ts
+++ tests/creatingListOfUsers.spec.ts
@@ -1,38 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { fa, faker } from '@faker-js/faker';
 import { z } from 'zod';
-import { postAPI, getAPI, putAPI, deleteAPI } from '../utils/apiCallHelper';
+import { postAPI, getAPI, putAPI, deleteAPI ,createRandomUser, createMultipleUserJson} from '../utils/apiCallHelper';
 test.describe('Create List of Users API tests', () => {
     const baseURL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
-    const createListOfUsersRequestBody =
-        [
-            {
-                id: 7463,
-                username: "string",
-                firstName: "string",
-                lastName: "string",
-                email: "string",
-                password: "string",
-                phone: "string",
-                userStatus: 7789
-            },
-            {
-                id: 1570,
-                username: "string",
-                firstName: "string",
-                lastName: "string",
-                email: "string",
-                password: "string",
-                phone: "string",
-                userStatus: 3369
-            }
-        ]
+    
     const expectedResponseSchemaZod = z.object({
         code: z.literal(200),
-        type: z.literal("unknown"), 
+        type: z.literal("unknown"),
         message: z.literal("ok")
     })
     test('create list of users', async ({ request }) => {
+        const createListOfUsersRequestBody = createMultipleUserJson(2);
         await postAPI(request, `${baseURL}/user/createWithArray`, createListOfUsersRequestBody, 200, expectedResponseSchemaZod, 5);
     })
 });
