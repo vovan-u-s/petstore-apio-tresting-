3e033749a5bd366ac247d344b43cfb93eab09f4e
vovan-u-s <oversoul6661@gmail.com>
2025-10-18 09:20:00 -0500

    nit done

--- tests/returnPet.spec.ts
+++ tests/returnPet.spec.ts
@@ -1,34 +1,16 @@
 import { test } from '@playwright/test';
 import { z } from 'zod';
 import { getAPI } from '../utils/apiCallHelper';
-
+   const expectedInventoryResponseSchema = z
+    .record(z.string(), z.number())
+    .refine((data) => Object.keys(data).length === 14, {
+      message: "Response must have exactly 14 keys",
+    });
 test.describe('Return Pet API tests', () => {
     const baseURL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
-    const FindByStatusSchema = z.array(
-        z.object({
-            id: z.number().int().positive(),
-            category: z.object({
-                id: z.number().int().positive(),
-                name: z.string().regex(/^[a-zA-Z0-9\s\-_]+$/)
-            }),
-            name: z.string().regex(/^[a-zA-Z0-9\s\-_]+$/),
-            photoUrls: z.array(
-                z.string().regex(/^https?:\/\/.+/)
-            ),
-            tags: z.array(
-                z.object({
-                    id: z.number().int().positive(),
-                    name: z.string().regex(/^[a-zA-Z0-9\s\-_]+$/)
-                })
-            ),
-            status: z.enum(['available', 'pending', 'sold'])
-        })
-    );
 
     test('validating of schema length', async ({ request }) => {
-        const response = await getAPI(request, `${baseURL}/store/inventory`, 200, z.record(z.number()), {}, 5);
-        const inventory = response;
-        const totalPets = Object.values(inventory).reduce((acc, curr) => acc + curr, 0);
-        console.log(`Total number of pets in inventory: ${totalPets}`);
+        await getAPI(request, `${baseURL}/store/inventory`, 200, expectedInventoryResponseSchema, 5);
+       
     })
 })
